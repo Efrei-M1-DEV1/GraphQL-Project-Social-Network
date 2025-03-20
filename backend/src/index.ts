0242044ab04d6f5aa891168f6658df61ecb6d1d3
georgyguei <georgy.guei@efrei.net>
2025-03-20 20:47:40 +0100

    :sparkles: feat: implement backend Apollo Server
    Added Apollo Server with hello query, Prisma context, and ESM support via register.js in backend/src/ and scripts/.

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import type { DataSourceContext } from "./context";
+import db from "./datasources/db.js";
+import { resolvers } from "./resolvers.js";
+import { typeDefs } from "./schema.js";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+  context: async () => {
+    return {
+      dataSources: {
+        db,
+      },
+    } satisfies DataSourceContext;
+  },
+});
+
+// biome-ignore lint/suspicious/noConsole: <explanation>
+console.log(`🚀 Server ready at ${url}`);
