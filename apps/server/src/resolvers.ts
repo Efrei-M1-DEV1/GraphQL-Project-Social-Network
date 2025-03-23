22d34d9647a83fad5cbfeaaf66451ce0b498d986
georgyguei <georgy.guei@efrei.net>
2025-03-23 19:42:30 +0100

    :sparkles: feat: define Prisma schema with enhanced models

--- apps/server/src/resolvers.ts
+++ apps/server/src/resolvers.ts
@@ -1,8 +1,14 @@
-export const resolvers = {
+import type { DataSourceContext } from "./context";
+import type { Resolvers } from "./types"; // Import generated types
+
+export const resolvers: Resolvers = {
   Query: {
     hello: async () => {
       await new Promise((resolve) => setTimeout(resolve, 5000));
       return "Hello, GraphQL!";
+    },
+    users: async (_parent, _args, context: DataSourceContext) => {
+      return context.dataSources.db.user.findMany();
     },
   },
 };
--- apps/server/src/schema.ts
+++ apps/server/src/schema.ts
@@ -1,5 +1,11 @@
 export const typeDefs = `#graphql
+  type User {
+    id: Int!
+    email: String!
+    name: String
+  }
   type Query {
     hello: String
+    users: [User!]!
   }
 `;
