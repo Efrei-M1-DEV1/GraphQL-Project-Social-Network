e5291af31779209892be2ad537cd64f6ba9ff78b
Farid-Efrei <128361230+Farid-Efrei@users.noreply.github.com>
2025-03-29 16:55:32 +0100

    feat: ajout du formulaire de connexion avec champs pour l'email et le mot de passe

--- apps/web/src/screens/login/page.tsx
+++ apps/web/src/screens/login/page.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@repo/ui/form/button";
+import { Field } from "@repo/ui/form/field";
+import { Link } from "@repo/ui/navigation/link";
+import { Heading } from "@repo/ui/typography/heading";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form className="mx-auto grid max-w-screen-sm gap-4 sm:grid-cols-2 ">
+      <Heading className="sm:col-span-2">Login</Heading>
+      <Field label="Email" helperText="Enter your email" errorText="Invalid email" required>
+        <input type="text" />
+      </Field>
+      <Field label="Password" helperText="Enter your password" required>
+        <input type="password" />
+      </Field>
+
+      <Button type="submit" className="bg-purple-600 sm:col-span-2">
+        Login
+      </Button>
+      <span className="text-xs">
+        Don't have an account?{" "}
+        <Link href="/register" className="text-purple-600">
+          Register
+        </Link>
+      </span>
+    </form>
   );
 }
