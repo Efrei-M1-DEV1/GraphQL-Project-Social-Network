78997d5e5330caa95d13a23d74e65f011e5b6b19
georgyguei <georgy.guei@efrei.net>
2025-04-06 20:27:00 +0200

    :recycle: refactor: enhance error handling and add retry functionality

--- apps/web/src/components/hello-message.tsx
+++ apps/web/src/components/hello-message.tsx
@@ -1,17 +1,38 @@
-import { gql, useSuspenseQuery } from "@apollo/client";
+import { graphql } from "@/gql";
+import type { HelloQuery } from "@/gql/graphql";
+import { useSuspenseQuery } from "@apollo/client";
+import { Button } from "@repo/ui/form/button";
+import { Icon } from "@repo/ui/media/icon";
+import { LuRefreshCcw } from "react-icons/lu";
 
-const HELLO_QUERY = gql`
+const HELLO_QUERY = graphql(`
   query Hello {
     hello
   }
-`;
+`);
 
 export default function HelloMessage() {
-  const { data, error } = useSuspenseQuery<{ hello: string }>(HELLO_QUERY);
+  const { data, error, refetch } = useSuspenseQuery<HelloQuery>(HELLO_QUERY, {
+    errorPolicy: "all",
+  });
+
+  const refetchData = () => {
+    refetch();
+  };
 
   if (error) {
-    return <p className="text-red-500">An error occurred: {error.message}</p>;
+    return (
+      <div className="flex items-center gap-1">
+        <p className="text-sm opacity-60">
+          Server says: <span className="text-[var(--colors-error)]">{error.message}</span>
+        </p>
+        <Button variant="link" onClick={refetchData} className="h-[inherit] p-0 [&>svg]:hover:animate-spin">
+          Retry
+          <Icon as={LuRefreshCcw} className="!duration-slowest rotate-180" />
+        </Button>
+      </div>
+    );
   }
 
-  return <p className="mt-2 text-gray-500 text-sm">Server says: {data.hello} 🚀</p>;
+  return <p className="text-sm opacity-60">Server says: {data?.hello} 🚀</p>;
 }
