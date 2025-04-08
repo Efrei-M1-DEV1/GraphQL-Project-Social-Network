ca8f4b10f08d878a657b44c714ec202c8eb58ee4
Farid-Efrei <128361230+Farid-Efrei@users.noreply.github.com>
2025-04-08 10:58:45 +0200

    refactor: replace static article data with GraphQL queries in ArticleList and ArticleDetails components

--- apps/web/src/screens/articles/[id]/page.tsx
+++ apps/web/src/screens/articles/[id]/page.tsx
@@ -1,42 +1,61 @@
+import { graphql } from "@/gql";
+import type { GetArticleQuery } from "@/gql/graphql";
+import { useQuery } from "@apollo/client";
 import { Card, CardBody, CardDescription, CardHeader, CardTitle } from "@repo/ui/data-display/card";
-import { Heading } from "@repo/ui/typography/heading";
 
-const articles = [
-  {
-    id: 1,
-    title: "Article 1",
-    description: "Detailed description of Article 1",
-  },
-  {
-    id: 2,
-    title: "Article 2",
-    description: "Detailed description of Article 2",
-  },
-  {
-    id: 3,
-    title: "Article 3",
-    description: "Detailed description of Article 3",
-  },
-];
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+const GET_ARTICLE = graphql(`
+  query GetArticle($id: Int!) {
+    article(id: $id) {
+      id
+      title
+      content
+      createdAt
+      author {
+        id
+        name
+      }
+    }
+  }
+`);
 
 export default function ArticleDetails(props: { params?: { id: string } }) {
-  const article = articles.find((article) => article.id === Number(props.params?.id));
+  const navigate = useNavigate();
+  const { id } = props.params || {};
 
-  if (!article) {
-    return (
-      <div className="mx-auto max-w-screen-md">
-        <Heading>Article not found</Heading>
-      </div>
-    );
+  const { loading, error, data } = useQuery<GetArticleQuery>(GET_ARTICLE, {
+    variables: {
+      id: Number(id),
+    },
+  });
+
+  useEffect(() => {
+    if (!loading && (error || !data || !data.article)) {
+      navigate("/error");
+    }
+  }, [navigate, error, data, loading]);
+
+  if (loading) {
+    return <p>Loading...</p>;
   }
+
+  if (error || !data || !data.article) {
+    return null;
+  }
+
   return (
     <div className="mx-auto max-w-screen-md">
       <Card>
         <CardHeader>
-          <CardTitle>{article.title}</CardTitle>
+          <CardTitle>{data.article.title}</CardTitle>
+          <CardDescription>
+            {data.article.author.name} - {data.article.createdAt}
+          </CardDescription>
         </CardHeader>
         <CardBody>
-          <CardDescription>{article.description}</CardDescription>
+          <p>{data.article.content}</p>
         </CardBody>
       </Card>
     </div>
